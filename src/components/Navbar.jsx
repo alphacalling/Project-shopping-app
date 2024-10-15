ff4edacd774d1370554e5095810bd8bc01aa7aff
Vikas Pandey <100843095+alphacalling@users.noreply.github.com>
2024-10-15 15:27:31 +0530

    Update Navbar.jsx
    css updation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,43 @@
-import {FaShoppingCart} from 'react-icons/fa';
+import { FaShoppingCart } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
-
 const Navbar = () => {
-
-    const {cart} = useSelector( (state) => state);
-
+  const { cart } = useSelector((state) => state);
 
   return (
-  <div >
-    <nav className="flex items-center justify-between h-20 max-w-6xl mx-auto ">
+    <div className="bg-gray-800 shadow-lg">
+      <nav className="flex items-center justify-between h-20 max-w-6xl mx-auto px-6">
 
-      <NavLink to='/'>
-        <div className='ml-5'>
-        <img src="../logo.png" className='h-14' alt='' />
-        </div>
-      </NavLink>
-
-      <div className='flex font-medium text-slate-100 items-center mr-5 space-x-6'>
-        <NavLink to='/'>
-        <p>Home</p>
+        {/* Logo */}
+        <NavLink to="/">
+          <div className="ml-5">
+            <img src="../logo.png" className="h-12 hover:opacity-90 transition-opacity" alt="Logo" />
+          </div>
         </NavLink>
 
-        <NavLink to='/cart'>
-        <div className='relative'>
-          <FaShoppingCart className='text-2xl'/>
-          {
-            cart.length > 0 &&
-            <span className='absolute -top-1 -right-2 bg-green-600 text-xs w-5 h-5 flex justify-center items-center
-            animate-bounce rounded-full text-white'>
-            {cart.length}</span>
-          }
-        </div>
-        </NavLink>
+        {/* Navigation Links */}
+        <div className="flex items-center space-x-8 text-white font-medium">
+          <NavLink to="/" className="hover:text-green-400 transition-colors duration-200">
+            Home
+          </NavLink>
 
-      </div>
-    </nav>
-  </div>
-  )
+          <NavLink to="/cart">
+            <div className="relative flex items-center">
+              <FaShoppingCart className="text-2xl text-white hover:text-green-400 transition-colors duration-200" />
+
+              {/* Cart Count Badge */}
+              {cart.length > 0 && (
+                <span className="absolute -top-2 -right-3 bg-green-600 text-xs w-5 h-5 flex items-center justify-center rounded-full text-white animate-bounce">
+                  {cart.length}
+                </span>
+              )}
+            </div>
+          </NavLink>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
