7d630739e21c50e83b7816ea3a568d06e28646c2
Vikas Pandey <100843095+alphacalling@users.noreply.github.com>
2024-10-15 15:25:33 +0530

    Update Home.jsx
    css updation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,46 +5,45 @@ import { useEffect, useState } from "react";
 const Home = () => {
   const API_URL = "https://fakestoreapi.com/products";
   const [loading, setLoading] = useState(false);
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
-  async function fetchProductData(){
+  async function fetchProductData() {
     setLoading(true);
-
-     try{
+    try {
       const result = await fetch(API_URL);
       const data = await result.json();
       setPosts(data);
-     }
-
-     catch(error){
+    } catch (error) {
       console.log("error aa gya");
       setPosts([]);
-     }
-     setLoading(false);
+    }
+    setLoading(false);
   }
 
-  useEffect( () => {
+  useEffect(() => {
     fetchProductData();
-  }, [])
+  }, []);
 
   return (
-    <div className="flex justify-center items-center mt-[100px]">
+    <div className="flex justify-center items-center mt-[100px] px-4">
       {
         loading ? <Spinner /> :
-        posts.length > 0 ? 
-        (<div className="grid xs:grid-cols-1 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
-        {
-          posts.map( (post) => (
-          <Product key= {post.id} post={post} />
-          ))
-        }
-        </div>) : 
-        <div className="flex justify-center items-center ">
-          <p>No Data Found</p>
-        </div>
-        }
-        </div>
-    );
+          posts.length > 0 ?
+            (<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-6xl mx-auto p-4 min-h-[80vh]">
+              {
+                posts.map((post) => (
+                  <div className="transform transition duration-300 hover:scale-105 hover:shadow-lg">
+                    <Product key={post.id} post={post} />
+                  </div>
+                ))
+              }
+            </div>) :
+            <div className="flex justify-center items-center h-full">
+              <p className="text-gray-600 text-lg font-semibold">No Data Found</p>
+            </div>
+      }
+    </div>
+  );
 };
 
 export default Home;
